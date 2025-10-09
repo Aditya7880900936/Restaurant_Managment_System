fa62fe0308529c7d294db71636e66441fcf99783
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-10-09 20:28:48 +0530

    feat(routes): add food routes with CRUD operations
    Implement basic CRUD endpoints for food items including:
    - GET /foods to retrieve all foods
    - GET /foods/:food_id for single food item
    - POST /foods to create new food
    - PATCH /foods/:food_id to update existing food

--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/Aditya7880900936/Restaurant_Managment_System/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controllers.GetFoods())
+	incomingRoutes.GET("/foods/:food_id" , controllers.GetFood())
+	incomingRoutes.POST("/foods",controllers.CreateFood())
+	incomingRoutes.PATCH("/foods/:food_id", controllers.UpdateFood())
+}
