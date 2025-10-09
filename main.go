460a66a75ca28807855385c50d680f47ce8235bc
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-10-09 20:24:12 +0530

    feat: implement user routes and main application setup
    - Add user routes with endpoints for getting users, single user, signup and login
    - Set up main application with MongoDB connection, middleware and route initialization
    - Add required dependencies in go.mod and go.sum for Gin and MongoDB

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/Aditya7880900936/Restaurant_Managment_System/database"
+	"github.com/Aditya7880900936/Restaurant_Managment_System/middlewares"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func main() {
-	fmt.Println("Hello world")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+    router.UserRoutes(router)
+	router.Use(middlewares.Authentication())
+	router.FoodRoutes(router)
+	router.InvoiceRoutes(router)
+	router.MenuRoutes(router)
+	router.OrderItemRoutes(router)
+	router.OrderRoutes(router)
+	router.TableRoutes(router)
+
+	router.Run(":" + port)
 }
