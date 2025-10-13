c273227273d40d8403e932fe6f123e71d2513697
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-10-14 00:21:42 +0530

    feat(menuController): implement update menu functionality
    Add logic to handle menu updates including date validation and field updates. The changes include:
    - Date range validation for start_date and end_date
    - Conditional updates for name, category, and timestamps
    - MongoDB update operation with upsert option

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -11,6 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
@@ -87,6 +88,66 @@ func CreateMenu() gin.HandlerFunc {
 
 func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+		var menu models.Menu
+
+		if err := c.BindJSON(&menu); err!= nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		menuId := c.Param("menu_id")
+
+		filter := bson.M{"menu_id": menuId}
+
+		var updateObj primitive.D
+
+		if menu.Start_Date!= nil && menu.End_Date!= nil {
+			if !inTimeSpan(*menu.Start_Date, *menu.End_Date, time.Now()) {
+				msg := "Kindly retype the time"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				defer cancel()
+				return
+			}
+
+			updateObj = append(updateObj, bson.E{"start_date", menu.Start_Date})
+			updateObj = append(updateObj, bson.E{"end_date", menu.End_Date})
+
+			if menu.Name!= "" {
+				updateObj = append(updateObj, bson.E{"name", menu.Name})
+			}
+
+			if menu.Category != "" {
+				updateObj = append(updateObj, bson.E{"created_at", menu.Created_At})
+			}
+
+			menu.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			updateObj = append(updateObj, bson.E{"updated_at", menu.Updated_At})
+
+			upsert := true
+
+			opt := options.UpdateOptions{
+				Upsert: &upsert,
+			}
+
+			result, err := menuCollection.UpdateOne(
+				ctx,
+				filter,
+				bson.D{
+					{"$set", updateObj},
+				},
+				&opt,
+			)
+
+			if err!= nil {
+				msg := "Menu update failed"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+
+			defer cancel()
+			c.JSON(http.StatusOK, result)
+		}
 	}
 }
