33f67f36fdd2eaf4c355c8ec320ab49f5c4af1c6
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-10-19 01:19:23 +0530

    feat(user): implement user controller and token helper functions
    - Add token helper functions (GenerateAllTokens, UpdateAllTokens, ValidateToken)
    - Implement GetUsers with pagination and aggregation pipeline
    - Implement GetUser with MongoDB query
    - Prepare Signup function structure with TODOs

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,22 +1,87 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/Aditya7880900936/Restaurant_Managment_System/database"
+	"github.com/Aditya7880900936/Restaurant_Managment_System/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Get all users route"})
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		recorsPerpage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recorsPerpage < 1 {
+			recorsPerpage = 10
+		}
+
+		page, err1 := strconv.Atoi(c.Query("page"))
+
+		if err1 != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recorsPerpage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		projectStage := bson.D{
+			{"$project", bson.D{
+				{"_id", 0},
+				{"total_count", 1},
+				{"user_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recorsPerpage}}}},
+			}},
+		}
+		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, projectStage,
+		})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
+		}
+		var allUsers []bson.M
+		if err = result.All(ctx, &allUsers); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allUsers[0])
 	}
 }
 
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Get user route"})
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		userId := c.Param("user_id")
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the user"})
+		}
+		c.JSON(http.StatusOK, user)
 	}
 }
 
 func Signup() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Signup route"})
+		//convert the JSON data into User struct
+		// validate the data based on user struct
+		// you'll check if the email exists or not
+		// hash the password
+		// you'll also check if the phone number exists or not
+		// create some extra details for the user object
+		// generate access token and refresh token
+		// insert the user object into the user collection
 	}
 }
 
