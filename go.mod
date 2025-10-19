f180941b64503f68b5b3d101ab7aff3a967147c1
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-10-20 00:08:51 +0530

    feat(auth): implement JWT token generation for authentication
    - Add jwt-go and crypto dependencies for token handling
    - Create SignedDetails struct to store user claims
    - Implement GenerateAllTokens function to create access and refresh tokens
    - Tokens expire in 24 hours and 7 days respectively
    - Use environment variable for secret key

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/Aditya7880900936/Restaurant_Managment_System
 go 1.24.3
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.11.0
 	github.com/go-playground/validator/v10 v10.27.0
 	go.mongodb.org/mongo-driver v1.17.4
+	golang.org/x/crypto v0.40.0
 )
 
 require (
@@ -38,7 +40,6 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.uber.org/mock v0.5.0 // indirect
 	golang.org/x/arch v0.20.0 // indirect
-	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/mod v0.25.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,7 +1,52 @@
 package helpers
 
-func GenerateAllTokens() {
+import (
+	"log"
+	"os"
+	"time"
 
+	"github.com/Aditya7880900936/Restaurant_Managment_System/database"
+	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstname string, lastname string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstname,
+		Last_name:  lastname,
+		Uid:        uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
 }
 
 func UpdateAllTokens() {
